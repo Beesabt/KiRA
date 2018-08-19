cd0c06fc82166844af4153a545ff382f68fe8de8
Eszti <beesabt@gmail.com>
2018-08-19 21:25:44 +0200

    [KiRA - Home.cs] - Method is removed because of a new logic

--- KiRA/BusinessLogicLayer/Home.cs
+++ KiRA/BusinessLogicLayer/Home.cs
@@ -9,17 +9,5 @@ namespace KiRA.BusinessLogicLayer
 {
     class Home
     {
-        DatabaseHandler _DBHandler;
-
-        public bool DefaultLoginValidation(string Username)
-        {
-            bool isSuccess = false;
-            string _password = string.Empty;     
-            _DBHandler = new DatabaseHandler();
-            _password = _DBHandler.GetPassword(Username);
-            if (LogInfo.UserName == "KiRA" || _password == "Admin") isSuccess = true;
-            return isSuccess;
-        }
-
     }
 }
